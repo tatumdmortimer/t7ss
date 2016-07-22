e313e809122e7532a6110913cb809ffb460787b9
Tatum Mortimer <Tatum Mortimer>
2016-07-22 16:57:56 -0500

    Updating tree plotting scripts

--- scripts/esx_treescape.R
+++ scripts/esx_treescape.R
@@ -2,23 +2,23 @@ library(treescape)
 
 eccA <- read.nexus("trees/mrbayes/eccA.nexus.run2.t")
 set.seed(123)
-eccAtrees <- eccA[sample(1:length(eccA),200)]
+eccAtrees <- eccA[sample(500:length(eccA),200)]
 eccB <- read.nexus("trees/mrbayes/eccB.nexus.run2.t")
-eccBtrees <- eccB[sample(1:length(eccB),200)]
-parA <- read.nexus("trees/mrbayes/parA.nexus.run2.t")
-parAtrees <- parA[sample(1:length(parA),200)]
-t4ss <- read.nexus("trees/mrbayes/t4ss.nexus.run2.t")
-t4sstrees <- t4ss[sample(1:length(t4ss),200)]
-zincFinger <- read.nexus("trees/mrbayes/zincFinger.nexus.run2.t")
-zincFingertrees <- zincFinger[sample(1:length(zincFinger),200)]
+eccBtrees <- eccB[sample(500:length(eccB),200)]
+espI <- read.nexus("trees/mrbayes/espI.nexus.run2.t")
+espItrees <- espI[sample(500:length(parA),200)]
+virB4 <- read.nexus("trees/mrbayes/virB4.nexus.run2.t")
+virB4trees <- virB4[sample(500:length(t4ss),200)]
+tcpC <- read.nexus("trees/mrbayes/tcpC.nexus.run2.t")
+tcpCtrees <- tcpC[sample(500:length(zincFinger),200)]
 
-trees <- c(eccAtrees, eccBtrees, parAtrees, t4sstrees, zincFingertrees)
+trees <- c(eccAtrees, eccBtrees, espItrees, virB4trees, tcpCtrees)
 class(trees) <- "multiPhylo"
 names(trees)[1:200] <- paste0("eccA",1:200)
 names(trees)[201:400] <- paste0("eccB",1:200)
 names(trees)[401:600] <- paste0("espI",1:200)
-names(trees)[601:800] <- paste0("t4ss",1:200)
-names(trees)[801:1000] <- paste0("zincFinger",1:200)
+names(trees)[601:800] <- paste0("virB4",1:200)
+names(trees)[801:1000] <- paste0("tcpC",1:200)
 
 trees_rooted <- lapply(trees, function(x) root(x, resolve.root=TRUE, outgroup="ERR340530"))
 class(trees_rooted) <- "multiPhylo"
@@ -27,3 +27,5 @@ scape <- treescape(trees_rooted,nf=6)
 dtype <- c(rep("EccA", 200),rep("EccB",200),rep("EspI",200),rep("VirB4",200),rep("TcpC",200))
 
 plotGroves(scape$pco, groups=dtype)
+plotGroves(scape$pco, groups=dtype, yax=3)
+plotGroves(scape$pco, groups=dtype, xax=2, yax=3)
--- scripts/plot_esx4_tree.R
+++ scripts/plot_esx4_tree.R
@@ -3,13 +3,7 @@ library(ape)
 library(phangorn)
 library(ggtree)
 
-tree <- read.tree("trees/RAxML_bipartitionsBranchLabels.esx4_combine")
-midpoint_rooted <- midpoint(tree)
-reordered_tree <- reorder(midpoint_rooted)
-p <- ggtree(midpoint_rooted) + geom_tiplab(size=3) + geom_treescale()
-p
-
-raxml_tree <- read.raxml("trees/RAxML_bipartitionsBranchLabels.esx4_midpointRoot.newick")
+raxml_tree <- read.raxml("trees/RAxML_bipartitionsBranchLabels_esx4.newick")
 ggtree(raxml_tree) + geom_label(aes(x=branch, label=bootstrap, fill=bootstrap)) + 
     geom_tiplab() +
     scale_fill_continuous(low='red', high='white') + 
