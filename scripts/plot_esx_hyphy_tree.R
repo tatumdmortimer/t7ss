a3a80e62ecff77ef15a98622a4fd1c16ae7dfd85
Tatum Mortimer <Tatum Mortimer>
2016-09-30 17:11:22 -0500

    Updated script to color branches based on the proportion of sites under selection

--- scripts/plot_esx_hyphy_tree.R
+++ scripts/plot_esx_hyphy_tree.R
@@ -9,24 +9,24 @@ midpoint_rooted <- midpoint(tree)
 reordered_tree <- reorder(midpoint_rooted)
 hyphyData <- read.table("hyphy/guidance/esx_absrel", sep=",", header=TRUE)
 hyphyResults <- read.table("hyphy/nodeLabels.txt")
-hyphyResults$omega <- hyphyData$OmegaOver1
+hyphyResults$omega <- hyphyData$WtOmegaOver1
 hyphyResults$p <- hyphyData$p_Holm
 hyphyResults$omega[hyphyResults$p > 0.05] <- 0
 hyphyResults$node <- NA
+hyphyResults$V1 <- as.character(hyphyResults$V1)
 tipnode <- seq_along(reordered_tree$tip.label)
 names(tipnode) <- reordered_tree$tip.label
 hyphyResults$node <- tipnode[hyphyResults$V1]
 i <- is.na(hyphyResults$node)
 hyphyResults$node[i] = hyphyResults$V1[i]
-p <- ggtree(reordered_tree, aes(color=log10(omega))) %<+% hyphyResults %>% flip(94,73) +
+p <- ggtree(reordered_tree, aes(color=as.factor(prop))) %<+% hyphyResults %>% flip(94,73) +
     geom_tiplab(size=3) +
     geom_cladelabel(node=96, label="ESX-1", align=T, offset=1) +
     geom_cladelabel(node=129, label="ESX-2", align=T, offset=1) +
     geom_cladelabel(node=106, label="ESX-3", align=T, offset=1) +
     geom_cladelabel(node=78, label="ESX-4", align=T, offset=1) +
     geom_cladelabel(node=122, label="ESX-5", align=T, offset=1) + 
-    geom_treescale() +
-    scale_color_viridis(option="plasma") +
-    theme(legend.position="right")
+    scale_color_manual(values=c("gray", "blue", "green", "red", "yellow", "pink"),
+                       labels=c("non-sig","<0.1", "0.1-0.2", "0.2-0.3","0.3-0.4",">0.4")) + theme(legend.position="right")
 
 
