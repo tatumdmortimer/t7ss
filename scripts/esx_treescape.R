c3e419c06441e85110c1cf90e8b33dcb858d39cf
Tatum Mortimer <Tatum Mortimer>
2016-09-30 17:11:43 -0500

    Added boxplot of distances

--- scripts/esx_treescape.R
+++ scripts/esx_treescape.R
@@ -3,6 +3,7 @@ library(phangorn)
 library("adegenet")
 library("adegraphics")
 library("ggplot2")
+library(reshape2)
 
 set.seed(123)
 eccA <- read.tree("trees/mrbayes/core1009.newick")
@@ -27,13 +28,13 @@ eccC <- read.tree("trees/mrbayes/core1004.newick")
 eccCtrees <- eccC[sample(1:length(eccC),200)]
 hyp1 <- read.tree("trees/mrbayes/core1006.newick")
 hyp1trees <- hyp1[sample(1:length(hyp1),200)]
-hyp2 <- read.tree("trees/mrbayes/core1012.newick")
-hyp2trees <- hyp2[sample(1:length(hyp2),200)]
+eccD <- read.tree("trees/mrbayes/core1012.newick")
+eccDtrees <- eccD[sample(1:length(eccD),200)]
 
 
 trees <- c(eccAtrees, eccBtrees, espItrees, virB4trees, 
            tcpCtrees, nrdHtrees, virDtrees, eccEtrees, 
-           mycPtrees, eccCtrees, hyp1trees, hyp2trees)
+           mycPtrees, eccCtrees, hyp1trees, eccDtrees)
 class(trees) <- "multiPhylo"
 names(trees)[1:200] <- paste0("eccA",1:200)
 names(trees)[201:400] <- paste0("eccB",1:200)
@@ -46,7 +47,7 @@ names(trees)[1401:1600] <- paste0("eccE",1:200)
 names(trees)[1601:1800] <- paste0("mycP",1:200)
 names(trees)[1801:2000] <- paste0("eccC",1:200)
 names(trees)[2001:2200] <- paste0("hyp1",1:200)
-names(trees)[2201:2400] <- paste0("hyp2",1:200)
+names(trees)[2201:2400] <- paste0("eccD",1:200)
 
 
 trees_rooted <- lapply(trees, function(x) midpoint(x))
@@ -55,8 +56,49 @@ scape <- treescape(trees_rooted,nf=13)
 
 dtype <- c(rep("EccA", 200),rep("EccB",200),rep("EspI",200),rep("VirB4",200),
            rep("TcpC",200),rep("NrdH",200),rep("VirD",200),rep("EccE",200),
-           rep("MycP",200),rep("EccC",200),rep("Hyp1",200),rep("Hyp2",200))
+           rep("MycP",200),rep("EccC",200),rep("Hyp1",200),rep("EccD",200))
 
 plotGroves(scape$pco, groups=dtype)
 plotGroves(scape$pco, groups=dtype, yax=3)
 plotGroves(scape$pco, groups=dtype, xax=2, yax=3)
+
+#calculate differences between secretion system gene trees and NrdH
+dist_mat <- multiDist(trees_rooted)
+d <- as.matrix(dist_mat)
+nrdH_dist <- as.vector(d[1001:1200,1:1000])
+nrdH_dist <- append(nrdH_dist, as.vector(d[1001:1200,1201:2400]))
+sec_dist <- as.vector(d[1:200,201:1000])
+sec_dist <- append(sec_dist, as.vector(d[1:200, 1201:2400]))
+sec_dist <- append(sec_dist, as.vector(d[201:400, 1:200]))
+sec_dist <- append(sec_dist, as.vector(d[201:400, 401:1000]))
+sec_dist <- append(sec_dist, as.vector(d[201:400, 1201:2400]))
+sec_dist <- append(sec_dist, as.vector(d[401:600, 1:400]))
+sec_dist <- append(sec_dist, as.vector(d[401:600, 601:1000]))
+sec_dist <- append(sec_dist, as.vector(d[401:600, 1201:2400]))
+sec_dist <- append(sec_dist, as.vector(d[601:800, 1:600]))
+sec_dist <- append(sec_dist, as.vector(d[601:800, 1201:2400]))
+sec_dist <- append(sec_dist, as.vector(d[601:800, 801:1000]))
+sec_dist <- append(sec_dist, as.vector(d[801:1000, 1:800]))
+sec_dist <- append(sec_dist, as.vector(d[801:1000, 1201:2400]))
+sec_dist <- append(sec_dist, as.vector(d[1201:1400, 1:1000]))
+sec_dist <- append(sec_dist, as.vector(d[1201:1400, 1401:2400]))
+sec_dist <- append(sec_dist, as.vector(d[1401:1600, 1:1000]))
+sec_dist <- append(sec_dist, as.vector(d[1401:1600, 1201:1400]))
+sec_dist <- append(sec_dist, as.vector(d[1401:1600, 1601:2400]))
+sec_dist <- append(sec_dist, as.vector(d[1601:1800, 1:1000]))
+sec_dist <- append(sec_dist, as.vector(d[1601:1800, 1201:1600]))
+sec_dist <- append(sec_dist, as.vector(d[1601:1800, 1801:2400]))
+sec_dist <- append(sec_dist, as.vector(d[1801:2000, 1:1000]))
+sec_dist <- append(sec_dist, as.vector(d[1801:2000, 1201:1800]))
+sec_dist <- append(sec_dist, as.vector(d[1801:2000, 2001:2400]))
+sec_dist <- append(sec_dist, as.vector(d[2001:2200, 1:1000]))
+sec_dist <- append(sec_dist, as.vector(d[2001:2200, 1201:2000]))
+sec_dist <- append(sec_dist, as.vector(d[2001:2200, 2201:2400]))
+sec_dist <- append(sec_dist, as.vector(d[2201:2400, 1:1000]))
+sec_dist <- append(sec_dist, as.vector(d[2201:2400, 1201:2200]))
+df <- data.frame(sec_dist, nrdH_dist)
+colnames(df) <- c(sec_name, nrd_name)
+df_melt <- melt(df)
+p <- ggplot(df_melt, aes(factor(variable), value)) + geom_boxplot() + theme_bw() + ylab("Tree Distance")
+
+
